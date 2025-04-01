ab162aebdbde0504b21aa7a704061ea3d3fbef9c
adrama <adrama@utexas.edu>
2025-03-31 19:32:02 -0500

    smoke test works

--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 Python 3.11.11
 GEMINI_API_KEY="some key" python gemini_chroma_rag.py
+pip install dotenv
+pip install google-genai
--- smoke_test.py
+++ smoke_test.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from google import genai
 
-client = genai.Client(api_key="")
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+print()
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
